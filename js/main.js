0cc47cb5609dd8be13e4d0aeed415a110bbf1f6e
Ivan Shulga <shg.ivan97@gmail.com>
2020-06-21 01:24:04 +0300

    create four slides with mobile

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ $(window).scroll(function(){
       $navHeader.removeClass('fixedNav');
     }
 });
+
+$('.burger').click(function() {
+  $('.burger').toggleClass('burger-active')
+  $('.header-right').toggleClass('mobile-menu')
+  $('.header-right .btn').toggleClass('btn--red')
+  $('.header-right .btn').toggleClass('mobile-btn')
+})
+
+$('.openPopup').click(function() {
+    $(".dark-fon, .popup-out").show()
+    $(".popup-out").css('display', 'flex')
+})
+
+$('.close-popup').click(function() {
+  $(".dark-fon, .popup-out").hide()
+})
